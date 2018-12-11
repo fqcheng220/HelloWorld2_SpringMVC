f34c4faf74b9096b5ec89cd5f36f5285f1037012
chengfeiqiang <feiqiang@eastmoney.com>
2018-12-11 19:18:15 +0800

    first commit

---
+++ src/com/fqcheng220/bean/User.java
@@ -0,0 +1,8 @@
+package com.fqcheng220.bean;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String mUserName;
+    public String mPwd;
+}
--- src/com/fqcheng220/controller/UserController.java
+++ src/com/fqcheng220/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fqcheng220.controller;
 
+import com.fqcheng220.bean.User;
 import com.fqcheng220.result.CommonResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,4 +13,38 @@ public class UserController {
         System.out.println("UserController login:"+userName+","+pwd);
         return new CommonResult<String>().status(0).body("hello");
     }
+
+    @RequestMapping(value = "/login1", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login1(@RequestBody String userName,
+                               @RequestBody String pwd){
+        System.out.println("UserController login:"+userName+","+pwd);
+        return new CommonResult<String>().status(0).body("hello");
+    }
+
+    @RequestMapping(value = "/login2", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login2(@RequestBody User user){
+        System.out.println("UserController login:"+user.mUserName+","+user.mPwd);
+        return new CommonResult<String>().status(0).body("hello");
+    }
+
+    //406
+    @RequestMapping(value = "/login3", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult login3(String userName,
+                               String pwd){
+        System.out.println("UserController login:"+userName+","+pwd);
+        return new CommonResult<String>().status(0).body("hello");
+    }
+
+    //406
+    @RequestMapping(value = "/login4", method = RequestMethod.POST,produces = {"application/x-www-form-urlencoded"})
+    @ResponseBody
+    public CommonResult login4(String userName,
+                               String pwd){
+        System.out.println("UserController login:"+userName+","+pwd);
+        return new CommonResult<String>().status(0).body("hello");
+    }
+
 }
